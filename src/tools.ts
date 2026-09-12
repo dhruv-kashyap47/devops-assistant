c67d814b8abbf6f046fc80cf883c3131661e1e3f
dhruv-kashyap47 <dhruv_kashyap47@outlook.com>
2026-09-12 18:16:53 +0530

    added schema and code for get_weather

--- src/tools.ts
+++ src/tools.ts
@@ -8,9 +8,21 @@ export async function get_weather(city: string): Promise<string> {
 
         const geoData = await geoRes.json();
 
-        if(!geoData.results || geoData.results.length == 0){
-            return `Could not fina a location named "${city}".`;
+        if(!geoData.results || geoData.results.length === 0){
+            return `Could not find a location named "${city}".`;
         }
+
+        const { latitude, longitude, name } = geoData.results[0];
+
+        const weatherRes = await fetch(
+            `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`
+        );
+
+        const weatherData = await weatherRes.json();
+
+        const temp = weatherData.current_weather?.temperature;
+        const wind = weatherData.current_weather?.windspeed;
+
     }
 }
 
