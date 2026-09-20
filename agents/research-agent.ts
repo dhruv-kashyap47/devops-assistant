be44cea6239993c550b7bdc9a082d19b216c10a1
dhruv-kashyap47 <dhruv_kashyap47@outlook.com>
2026-09-20 10:42:48 +0530

    added research-agent

--- agents/research-agent.ts
+++ agents/research-agent.ts
@@ -17,3 +17,31 @@ type Message = {
     tool_call_id?: string,
     name?: string,
 }
+
+async function web_search(query: string): Promise<string> {
+    const response = await tvly.search(query, { maxResults: 5 });
+
+    return response.results.map((r) => `- ${r.title}\n  ${r.content}\n  Source: ${r.url}`).join("\n\n")
+};
+
+const tools = [
+  {
+    type: "function" as const,
+    function: {
+      name: "web_serach",
+      description: "Search the web for the current information on a topic.",
+      parameters: {
+        type: "object",
+        properties: {
+          query: {
+            type: "string",
+            description: "The search query.",
+          },
+        },
+        required: ["query"],
+      },
+    },
+  },
+];
+
+
