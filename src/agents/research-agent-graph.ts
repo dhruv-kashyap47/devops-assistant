ae8f575f2bbd3b9f1c61d5cab654be15efd7979a
dhruv-kashyap47 <dhruv_kashyap47@outlook.com>
2026-09-25 13:00:44 +0530

    initiated complete rewrite of agents in langgraph

--- src/agents/research-agent-graph.ts
+++ src/agents/research-agent-graph.ts
@@ -9,4 +9,35 @@ import { tavily } from "@tavily/core";
 
 const tvly = tavily({ apiKey: process.env.TAVILY_API_KEY });
 
+const webSearchTool = tool(
+  async ({ query }) => {
+    const response = await tvly.search(query, { maxResults: 4 });
+    return response.results
+      .map((r) => `- ${r.title}\n ${r.content}\n Source: ${r.url}`)
+      .join("\n\n");
+  },
+  {
+    name: "web_search",
+    description: "Search the web for current information on a topic.",
+    schema: z.object({
+        query: z.string().describe("The search query"),
+    })
+  },
+);
+
+const model = new ChatOpenAI({
+  apikey: process.env.OPENROUTER_API_KEY,
+  model: "openrouter/free",
+  configuration: {
+    baseURL: "https://openrouter.ai/api/v1",
+  },
+});
+
+const availableTools = model.bindTools([webSearchTool]);
+
+async function callModel(memory_state: typeof MessagesAnnotation.State){
+    const response = await availableTools.invoke(memory_state.messages);
+    return { messages: [response] };
+}
+
 
